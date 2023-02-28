13d6c0ceb86ddc6d64d85787ad7db81d8625c70f
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-02-28 20:58:53 +0530

    not prevall prevUntil

--- cypress/integration/Traverse/TC07TraverseD5.spec.js
+++ cypress/integration/Traverse/TC07TraverseD5.spec.js
@@ -9,17 +9,16 @@ describe('Verify the cypress traverse methods', () => {
 
     it('To get parents DOM element of elements, use the .parents() command.', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-        cy.get('[href="#/offers"]').parents().should('have.class','cart')
-        cy.get('[href="#/offers"]').parents().should('have.class','container')
-        cy.get('[href="#/offers"]').parent().should('have.class','cart')
-        cy.get('.cart').parents().should('have.length',6)
-        cy.get('.mobile-search').parents().should('have.length',7)
+        cy.get('[href="#/offers"]').parents().should('have.class', 'cart')
+        cy.get('[href="#/offers"]').parents().should('have.class', 'container')
+        cy.get('[href="#/offers"]').parent().should('have.class', 'cart')
+        cy.get('.cart').parents().should('have.length', 6)
+        cy.get('.mobile-search').parents().should('have.length', 7)
     })
 
-    it.only('To get parents DOM element of elements until other element, use the .parentsUntil() command.',()=>{
+    it.only('To get parents DOM element of elements until other element, use the .parentsUntil() command.', () => {
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/')
-        cy.get('[href="#/offers"]').parentsUntil('#root').should('have.length',4)
-        cy.get('.cart-info').parentsUntil('#root').should('have.length',4)
-
+        cy.get('[href="#/offers"]').parentsUntil('#root').should('have.length', 4)
+        cy.get('.cart-info').parentsUntil('#root').should('have.length', 4)
     })
 })
