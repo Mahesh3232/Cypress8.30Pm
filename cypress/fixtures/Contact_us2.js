bdd638b4dbd01ba9544e6826cbde5a319ff7770e
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-03-10 21:08:26 +0530

    Fixture D3

---
+++ cypress/fixtures/Contact_us2.js
@@ -0,0 +1,22 @@
+let info = [
+    {
+        "name": "Mahesh Aher",
+        "Email": "ABC@gmail.com",
+        "msg": "I am New in Cypress",
+        "Smsg": "Form submitted successfully."
+    },
+    {
+        "name": "Akash Aher",
+        "Email": "Xyz@gmail.com",
+        "msg": "I am New in js",
+        "Smsg": "Form submitted successfully."
+    },
+    {
+        "name": "Rushi Aher",
+        "Email": "PQR@gmail.com",
+        "msg": "I am New in Java",
+        "Smsg": "Form submitted successfully."
+    }
+]
+
+export default info
