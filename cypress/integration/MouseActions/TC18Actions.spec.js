2a98cd54e8a41b9e9486e72ee6bd519341024a33
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-03-16 21:09:33 +0530

    actions

---
+++ cypress/integration/MouseActions/TC18Actions.spec.js
@@ -0,0 +1,32 @@
+//MouseActions 
+//Click
+//RigthClick
+//DoubleClick
+//MouseOver
+//MouseMove
+//MouseUp
+//MouseDown
+//Scrool
+
+
+
+describe('Verify the mouse actions',()=>{
+
+    beforeEach(()=>{
+        cy.visit('https://testpages.herokuapp.com/styled/drag-drop-javascript.html')
+    })
+    
+    it('Verify the scroll action',()=>{
+        cy.get('[href="https://eviltester.com"]').scrollIntoView().click()
+    })
+
+    it('Removeing _black value',()=>{
+        cy.get('[href="https://eviltester.com"]').invoke('removeAttr','target').click()
+    })
+
+    it.only('Verify mousedown,mouseup & mouseMove',()=>{
+        cy.get('#draggable1').trigger('mousedown',{which:1})
+        cy.get('#droppable1').trigger('mousemove').trigger('mouseup',{force: true})
+        cy.get('#droppable1 > p').should('have.text','Dropped!')
+    })
+})
