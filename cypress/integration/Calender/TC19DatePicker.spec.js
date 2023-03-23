6a8922da06c5c0db36a4d4cb3160c15ae121ac3c
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-03-23 21:11:19 +0530

    Calender

--- cypress/integration/Calender/TC19DatePicker.spec.js
+++ cypress/integration/Calender/TC19DatePicker.spec.js
@@ -1,22 +1,34 @@
-
-
 describe('Verify the date picker functionality',()=>{
 
     it('Verify Calender through cypress',()=>{
         let date = new Date()
-        date.setDate(date.getDate() + 200)
+        date.setDate(date.getDate() + 300)
         let da = date.getDate()// current date
         let month = date.getMonth()//Return current month index
         //jan = 0 feb = 1 march = 2
         let fullMonth = date.toLocaleString('default',{month:"long"})
         let fullMonth2 = date.toLocaleString('default',{month:"short"})
         let year = date.getFullYear()
-      
         cy.log(da)
         //cy.log(month)
         cy.log(fullMonth)
         //cy.log(fullMonth2)
         cy.log(year)
+        cy.visit('https://demo.automationtesting.in/Datepicker.html')
+        cy.get('#datepicker1').click()
+        function selectMonthYear(){
+            cy.get('.ui-datepicker-title').then((el)=>{
+                cy.log(el.text())
+                cy.log(typeof(el.text()))
+                if(!el.text().includes(year)){
+                    cy.get('.ui-datepicker-next.ui-corner-all').click()
+                    selectMonthYear()
+                }
+            })
+
+        }
+        selectMonthYear()
+
 
     })
 })
