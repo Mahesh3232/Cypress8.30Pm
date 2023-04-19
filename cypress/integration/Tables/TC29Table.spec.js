fe92d749ab6d249f85a9b73bf01915e90b2d3c19
Mahesh3232 <mahesh.aher1999@gmail.com>
2023-04-19 21:13:46 +0530

    TAble Added

--- cypress/integration/Tables/TC29Table.spec.js
+++ cypress/integration/Tables/TC29Table.spec.js
@@ -1,9 +1,13 @@
 
 
+
 describe('Validate the tables', () => {
+
+    beforeEach(()=>{
+        cy.visit('https://testautomationpractice.blogspot.com/')
+    })
     let sum = 0
     it('Verify the total sum price colum', () => {
-        cy.visit('https://testautomationpractice.blogspot.com/')
         cy.get('[name="BookTable"]').find('tbody').children().each((el, index) => {
             //cy.log(el.text())
             if (index != 0) {
@@ -12,13 +16,24 @@ describe('Validate the tables', () => {
                 //cy.log(typeof(Number(el.children('td').last().text())))
                 sum = sum + Number(el.children('td').last().text())
             }
-           
-        }).then(()=>{
+        }).then(() => {
             expect(sum).to.eq(7100)
             //cy.log(sum)
         })
-       
     })
 
-    
+    it.only('Verify the Authers Name from the table',()=>{
+        //                 0      1        2         3       4       5
+        let autherName = ['','Amit','Mukesh','Animesh','Mukesh','Amod','Amit']
+        cy.get('[name="BookTable"]').find('tbody').children().each((el,index)=>{
+            //cy.log(el.text())
+            if(index != 0 ){
+                cy.get(el).children().eq(1).should('have.text',autherName[index])
+            }      
+        })
+    })
+
+
+
+
 })
